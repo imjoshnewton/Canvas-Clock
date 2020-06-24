c6830491ae17339ad4abb7e8b63a4829d9cb37a2
Josh Newton <imjoshnewton@users.noreply.github.com>
2020-06-24 16:20:41 -0400

    add transparency

--- source/js/main.js
+++ source/js/main.js
@@ -169,10 +169,14 @@ function animate() {
   fontSize = smallDim * 0.29 > 65 ? ((smallDim * 0.29) / 5) * 2.3 : 30;
 
   c.clearRect(0, 0, canvas.width, canvas.height);
+  
+  c.globalAlpha = 0.75;
 
   seconds.update(scAngle); // + 0.01);
   minutes.update(mnAngle); // + 0.01);
   hours.update(hrAngle); // + 0.01);
+  
+  c.globalAlpha = 1;
 
   c.textBaseline = "middle";
   c.textAlign = "center";
